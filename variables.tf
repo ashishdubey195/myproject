9dfb2bab233ad970c092c3c90fbe227c690c6c80
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-26 13:38:57 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+variable "aws_type" {
+  description = "AWS ec2 type"
+  default = "t3.micro"
+}
+variable "aws_ami" {
+  describtion = " AWS ami"
+  default = "ami-0a232144cf20a27a5"
+}
+
+
+
