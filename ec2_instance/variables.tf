bb0be167d245817755f02a19cb36a13a750f0a3d
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-26 13:23:53 +0100

    Create variables.tf

---
+++ ec2_instance/variables.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+  describtion = "The AMI ID to use for the EC2 Instance"
+}
+
+variable "instance_type" {
+  description = "the instance type for EC2 instance"
+}
+variable "instance_name"{
+  describtion = "the name of EC2 instance"
+}
