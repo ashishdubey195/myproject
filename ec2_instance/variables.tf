a9956047a5376ef68bf1ddb4e542927d131bfe9f
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-27 12:15:38 +0100

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,10 +1,10 @@
 variable "ami" {
-  describtion = "The AMI ID to use for the EC2 Instance"
+  description = "The AMI ID to use for the EC2 Instance"
 }
 
 variable "instance_type" {
   description = "the instance type for EC2 instance"
 }
 variable "instance_name"{
-  describtion = "the name of EC2 instance"
+  description = "the name of EC2 instance"
 }
